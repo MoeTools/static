74f3ccc52ced6515b635b07dbe2c3abee54860cd
MoeTools <61424424+MoeTools@users.noreply.github.com>
2020-03-08 19:35:39 +0800

    Upload file

---
+++ js/wa.js
@@ -0,0 +1,21 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.async = 1;
+    hm.src = "//hm.baidu.com/hm.js?59582f138c7bb3052a02d2052054627a";
+    document.body.appendChild(hm);
+})();
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-120453271-3', 'auto');ga('send', 'pageview');
+
+(function() {
+    var ad = document.createElement("script");
+    ad.setAttribute("data-ad-client", "ca-pub-3698194630053816");
+    ad.async = 1;
+    ad.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+    document.body.appendChild(ad);
+})();
